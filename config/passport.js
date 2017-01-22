945f7e197f0a3276078446dad30978820106de2c
djekoff <djekoff@gmail.com>
2017-01-22 21:25:27 +0300

    [+] Авторизация

--- config/passport.js
+++ config/passport.js
@@ -50,3 +50,35 @@ passport.use('local.signup', new LocalStrategy({
         });
     });
 }));
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Неверный формат email').notEmpty().isEmail();
+    req.checkBody('password', 'Неверный пароль').notEmpty().isLength({min: 4});
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(err) {
+            messages.push(err.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({'email': email}, function(err, user) {
+        if (err) {
+            return done(err);
+        }
+
+        if (!user) {
+            return done(null, false, {message: 'Пользователь не найден'});
+        }
+
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Неверный пароль'});
+        }
+
+        return done(null, user);
+    });   
+}))
