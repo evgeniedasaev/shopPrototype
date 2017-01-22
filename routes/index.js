af4e51abfab4f5b8b65e8775857c5f6170506429
djekoff <djekoff@gmail.com>
2017-01-22 19:39:47 +0300

    [*] Регистрация нового пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 var Product = require('../models/product');
 
@@ -15,6 +20,21 @@ router.get('/', function(req, res, next) {
 
     res.render('index', { title: 'Магазин', product_chunks: chunks });
   }); 
+});
+
+router.get('/user/signup', function(req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
+
+router.get('/user/profile', function(req, res, next) {
+  res.render('user/profile');
 });
 
 module.exports = router;
