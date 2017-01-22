28b7b51065061b704858a8c5385c1cde1a94336a
djekoff <djekoff@gmail.com>
2017-01-22 21:16:07 +0300

    [+] Добавлена валидация данных при регистрации

--- config/passport.js
+++ config/passport.js
@@ -18,6 +18,16 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, function(req, email, password, done) {
+    req.checkBody('email', 'Неверный формат email').notEmpty().isEmail();
+    req.checkBody('password', 'Неверный пароль').notEmpty().isLength({min: 4});
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(err) {
+            messages.push(err.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
     User.findOne({'email': email}, function(err, user) {
         if (err) {
             return done(err);
