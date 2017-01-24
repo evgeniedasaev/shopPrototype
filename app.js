82f0fa06dea0b5deae1ca1beb818ef6d5e0990c9
djekoff <djekoff@gmail.com>
2017-01-24 15:38:57 +0300

    [+] Подключаем хранение сессий в MongoDB

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var MongoStore = require('connect-mongo');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
@@ -29,7 +30,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'djxj9TUY', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'djxj9TUY', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -37,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
